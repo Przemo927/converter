43e18a06a9db74d13a84839900103da52b045a18
Przemek <przemo927@wp.pl>
2019-02-24 13:52:09 +0100

    Change configuration of logger.

--- src/main/java/pl/converter/converter/configuration/LoggerConfiguration.java
+++ src/main/java/pl/converter/converter/configuration/LoggerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
@@ -15,7 +16,7 @@ import java.util.logging.SimpleFormatter;
 @Configuration
 public class LoggerConfiguration {
 
-    private static final String NAME_LOG_FILE="ProjektLog.log";
+    private static final String NAME_LOG_FILE="ProjectLog.log";
     private static FileHandler fh;
     private static Logger logger;
     private final static String LOG_PATH=System.getProperty("user.dir")+ File.separator+"log"+File.separator+NAME_LOG_FILE;
@@ -25,17 +26,21 @@ public class LoggerConfiguration {
     public Logger logger(InjectionPoint injectionPoint) {
         try {
             logger = Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-            if(fh==null){
-                fh = new FileHandler(LOG_PATH);
-                fh.setLevel(Level.ALL);
-                logger.addHandler(fh);
-                SimpleFormatter formatter = new SimpleFormatter();
-                fh.setFormatter(formatter);
-            }
+            File file = new File(LOG_PATH);
+            file.getParentFile().mkdirs();
+            fh = new FileHandler(LOG_PATH);
+            fh.setLevel(Level.ALL);
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
         } catch (SecurityException | IOException e) {
             logger.log(Level.SEVERE,"Logger exception",e);
         }
         return logger;
     }
 
+    @PreDestroy
+    public void closeFileHandler(){
+        fh.close();
+    }
 }
