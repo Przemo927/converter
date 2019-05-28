f1692d7f505dfd5bfdac815fe205eed4737d6f57
Przemek <przemo927@gmail.com>
2019-05-28 18:33:23 +0200

    Add addition functions to converter builder

--- angular/src/app/converter/converter.ts
+++ angular/src/app/converter/converter.ts
@@ -1,14 +1,23 @@
 import {ZoomableElement} from "./zoomableelement";
 import {DraggableElement} from "./draggableelement";
+import {PatternElement} from "./patternelement";
+import {StringUtils} from "../stringutils";
 
 export class Converter {
 
   private element;
+  private container;
   private zoom = false;
   private dragg = false;
+  private pattern = false;
 
   public static builder() {
     return new Converter();
+  }
+
+  public setContainer(element: HTMLElement): Converter {
+    this.container = element;
+    return this;
   }
 
   public setImage(element: HTMLElement): Converter {
@@ -26,12 +35,20 @@ export class Converter {
     return this;
   }
 
+  public addFunctionalityOfPattern(): Converter {
+    this.pattern = true;
+    return this;
+  }
+
   public build() {
     if (this.zoom) {
       new ZoomableElement(this.element);
     }
     if (this.dragg) {
       new DraggableElement(this.element);
+    }
+    if(this.pattern){
+      new PatternElement(this.container, this.element, document.getElementById(StringUtils.ADD_PATTERN), document.getElementById(StringUtils.REMOVE_PATTERN));
     }
   }
 }
