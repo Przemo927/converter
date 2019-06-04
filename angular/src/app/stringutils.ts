2368bae9a05ebf54cff3b7ba22b3834609e9c2f1
Przemek <przemo927@gmail.com>
2019-06-04 22:25:30 +0200

    Add dot as static variable.

--- angular/src/app/stringutils.ts
+++ angular/src/app/stringutils.ts
@@ -3,6 +3,7 @@ export class StringUtils {
   public static EMPTY = "";
   public static PX = "px";
   public static COMMA = ",";
+  public static DOT = ".";
   public static LEFT_BRACKET = "(";
   public static ADD_PATTERN = "addPattern";
   public static REMOVE_PATTERN="removePattern";
